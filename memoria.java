028bc04caabff8c694c9cc002715b422b8ed189e
Arthur Maciel <84238286+ArthurMaciel120119@users.noreply.github.com>
2022-04-25 16:31:02 -0300

    Update memoria.java

--- memoria.java
+++ memoria.java
@@ -1,12 +1,13 @@
 public class memoria {
-    private String[] instrucao;
     private String[] memoria;
-    private String[] codigoop;
-    private String[] endereco;
-    private String[] dado;
+    private String dado;
 
-    public String[] lerEndereco(String[] endereco) {
-        System.out.println("Memoria lendo instrucao no Ciclo de busca e enviando para MBR o dado");
-        return dado;
+    public String lerEndereco(String palavra) {
+        int i=0;
+        System.out.println("Memoria lendo instrucao e guardando no Ciclo de busca e enviando o dado para MBR");
+        memoria = new String[32];
+        memoria[i] = palavra;
+        i++;
+        return palavra;
     }
 }
